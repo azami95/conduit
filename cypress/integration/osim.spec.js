e0b7fc5439b2ab0f395b72a485f2c427172cdfee
Osim Khusainov <keedice@gmail.com>
2022-07-10 15:27:59 -0400

    osims changes

--- cypress/integration/osim.spec.js
+++ cypress/integration/osim.spec.js
@@ -1,32 +1,28 @@
+describe("Articles lenght", () => {
+  it("", () => {
+    cy.intercept("GET", "https://api.realworld.io/api/articles?limit*").as(
+      "osim1"
+    );
+    cy.logInToApp("azami.95@mail.ru", "postavshik1995");
 
+    cy.contains("Global Feed").click();
 
-describe ('Articles lenght', () => {
+    cy.get("app-article-list")
+      .find("app-article-preview")
+      .then((locator) => {
+        cy.wrap(locator).should("have.length", 10);
 
+        cy.wait("@osim1").then((allParams) => {
+          cy.wrap(locator).should(
+            "have.length",
+            allParams.response.body.articlesCount
+          );
+        });
+      });
+  });
 
-    it ('', () => {
-
-        cy.intercept('GET', 'https://api.realworld.io/api/articles?limit*').as('osim1')
-        cy.logInToApp('azami.95@mail.ru', 'postavshik1995')
-
-        cy.contains('Global Feed').click()
-
-        cy.get('app-article-list').find('app-article-preview')
-        .then(locator => {
-            cy.wrap(locator).should('have.length', 10)
-
-        cy.wait('@osim1')
-        .then(allParams => {
-            cy.wrap(locator).should('have.length', allParams.response.body.articlesCount)
-        })    
-
-
-        
-        })
-
-
-    })
-
-    it ('', () => {
-        cy.contains('azami')
-    })
-})
+  it("", () => {
+    cy.contains("azami");
+    cy.contains("osim");
+  });
+});
